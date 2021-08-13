01efd36aa05e7613ccd613398947c0d52d506058
suryamanilodhi <57284410+suryamanilodhi@users.noreply.github.com>
2021-08-13 10:39:40 +0530

    program  using if elsse
    if else

---
+++ if else.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namesapace std;
+int main()
+{
+int savings;
+cin>>savings;
+if{
+(savings>5000)
+if (savings>2000){
+cout<<"road trip with neha\n";
+}
+else {
+cout<<"road trip with som\n";
+}
+}
+else{
+if(savings<1000){
+cout<<"bakchodi with friend\n";
+}
+reeturn(0);
+}
+
