4bcdd32c2bebfe6210e08988169409ba29e28896
suryamanilodhi <57284410+suryamanilodhi@users.noreply.github.com>
2021-08-13 10:37:39 +0530

    Add files via upload

---
+++ if .cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int savings;
+cin>>savings;
+if(savings>5000)
+{
+if(savings>10000)
+{
+cout<<"roadtrip with neha";
+}
+else
+{
+cout<<"shopping with neha\n";
+}
+}
+else
+if (savings>2000)
+{
+cout<<"rashmi\n";
+}
+else
+{
+cout<<" friends\n";
+}
+return(0);
+}
