1076656389f953ec5725be26029dda9b9158df8a
suryamanilodhi <57284410+suryamanilodhi@users.noreply.github.com>
2021-08-25 10:48:21 +0530

    program using structure

---
+++ struct.c
@@ -0,0 +1,15 @@
+struct student
+{
+int roll no;
+float per;
+char grade;
+};
+void main()
+{
+struct student
+s.roll=101;
+s.per=75.5;
+s.grade=A;
+printf(" roll no=%d percentage=%f grade=%c s.roll no,s.per,s.grade");
+getch();
+};
