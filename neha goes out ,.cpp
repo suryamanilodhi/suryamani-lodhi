7580a0e77d7c31b25948afd9e4a2911dfb24f4f3
suryamanilodhi <57284410+suryamanilodhi@users.noreply.github.com>
2021-08-25 10:45:15 +0530

    neha goes out on even days

---
+++ neha goes out ,.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int pocket money=3000;
+for(date=1; date<=30;date++)
+{
+if(date%2==0){
+continue;
+}
+if pocket money==0)
+{
+break;
+}
+cout<<"go out today"<<endl;
+pocket money=pocketmoney-3000;
+}
+return(0);
+}
