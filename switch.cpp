b9a8469e5bc89fff73d3677fac4ef34f7696f19e
suryamanilodhi <57284410+suryamanilodhi@users.noreply.github.com>
2021-09-06 22:58:41 +0530

    switch case.cpp

---
+++ switch.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int button;
+switch(button)
+    {
+    case 'a':
+        cout<<"hello"<<endl;
+        break;
+    case 'b':
+        cout<<"namaste"<<endl;
+        break;
+    case 'c':
+        cout<<"salut"<<endl;
+    break;
+    case 'd':
+        cout<<"hola"<<endl;
+    break;
+    case 'e':
+        cout<<"pranam"<<endl;
+        break;
+        deafult:
+            cout<<"i am still learning more"<<endl;
+            break;
+    }
+return 0;
+}
