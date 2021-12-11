f0ba458cf12cda8fe57afbb5bcbfccd6ec1ed408
suryamani lodhi <57284410+suryamanilodhi@users.noreply.github.com>
2021-12-11 12:03:41 +0530

    no is prime or not prime using python
    prime or not prime

---
+++ prime.py
@@ -0,0 +1,11 @@
+num=int(input("enter the number:"))
+prime=True
+for i in range(2,num):
+    if(num%i==0):
+
+        prime=False
+    
+    if prime:
+        print("this number is prime")
+    else:
+        print("this number is not prime")
